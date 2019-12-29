8989bb15ea955d4ce25f787877c7f8727ec49c02
unknown <mattialeoni@gmail.com>
2019-12-29 13:41:20 +0100

    Solved an error in Item Component

--- src/App.js
+++ src/App.js
@@ -6,18 +6,42 @@ import TodoList from "./components/TodoList";
 // VS -code github setup
 class App extends Component {
   state = {
-    items: [
-      { id: 1, title: "wake up" },
-      { id: 2, title: "wake breakfast" }
-    ],
+    items: [],
     id: uuid(),
     item: "",
     editItem: false
   };
-  handleChange = e => {};
-  handleSubmit = e => {};
-  clearList = () => {};
-  handleDelete = id => {};
+  handleChange = e => {
+    this.setState({
+      item: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item
+    };
+    const updatedItems = [...this.state.items, newItem];
+
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: uuid(),
+      editItem: false
+    });
+  };
+  clearList = () => {
+    this.setState({
+      items: []
+    });
+  };
+  handleDelete = id => {
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    this.setState({
+      items: filteredItems
+    });
+  };
   handleEdit = id => {};
   render() {
     return (
