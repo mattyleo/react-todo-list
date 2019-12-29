69ad8979766f2b274e7c1be2955d65c6b70c5a06
unknown <mattialeoni@gmail.com>
2019-12-29 11:35:51 +0100

    add the icons and setting up the "Todo Input"

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,6 +2,32 @@ import React, { Component } from "react";
 
 export default class TodoList extends Component {
   render() {
-    return <h1>hello fdrom todo list</h1>;
+    const { item, handleChange, handleSubmit, editItem } = this.props;
+    return (
+      <div className="card card-body my-3">
+        <form onSubmit="{handleSubmit}">
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fas fa-book"></i>
+              </div>
+            </div>
+            <input
+              type="text"
+              className="form-control text-capitlize"
+              placeholder="add todo item"
+              value={item}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-block btn-primary mt-3 text-upercase"
+          >
+            add item
+          </button>
+        </form>
+      </div>
+    );
   }
 }
